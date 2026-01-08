4a2de4f1e031bcf3ba796d145eb17b79ae3b1a84
yyc285184831 <yyc285184831@gmail.com>
2026-01-08 13:53:39 +0900

    update

--- database.py
+++ database.py
@@ -4,18 +4,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 优先尝试从 secrets 获取云端数据库地址，没有则使用本地 SQLite
-try:
-    # 在 secrets.toml 中配置 [connections.postgresql] dialect="postgresql" ...
-    # 或者直接存一个字符串:
-    # [database]
-    # url = "postgresql://..."
-    SQLALCHEMY_DATABASE_URL = st.secrets["database"]["url"]
-    # Postgres 需要这个参数
-    connect_args = {} 
-except:
-    # 本地回退方案
-    SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
-    connect_args = {"check_same_thread": False}
+# try:
+# 在 secrets.toml 中配置 [connections.postgresql] dialect="postgresql" ...
+# 或者直接存一个字符串:
+# [database]
+# url = "postgresql://..."
+SQLALCHEMY_DATABASE_URL = st.secrets["database"]["DATABASE_URL"]
+# Postgres 需要这个参数
+connect_args = {} 
+# except:
+#     # 本地回退方案
+#     SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+#     connect_args = {"check_same_thread": False}
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args=connect_args
